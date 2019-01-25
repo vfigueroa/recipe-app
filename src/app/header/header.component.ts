a65b36daf5e95912c866f7d8f7b539227aa97e34
Victor F <victor@figuerroa.com>
2019-01-25 18:39:27 -0500

    Added Recipe, Recipe list, Recipe Detail, Shopping list, and Shopping edit components as well as their respective bootstrap layout

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { headersToString } from 'selenium-webdriver/http';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: 'header.component.html',
+})
+export class HeaderComponent {
+
+}
